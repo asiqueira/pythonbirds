aa7a08201ee91fcc3444aab9e07e885f75f933b5
Alexandre Siqueira <email.alexandre.siqueira@gmail.com>
2020-09-07 15:38:47 -0300

    Criado atributo complexo filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=37):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Alexandre')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Xandinho'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+
+    for filho in luciano.filhos:
+        print(filho.nome)
